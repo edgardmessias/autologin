68f803bacb68c276dd0620c82ccd885aed792c11
Edgard Lorraine Messias <edgardmessias@gmail.com>
2016-12-31 16:34:59 -0200

    First Release;

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# autologin
-Auto Login - Skip the login page if you are already authenticated in GLPI
+# Auto Login (GLPI)
+This plugin skip the login page if you are already authenticated in GLPI
---
+++ setup.php
@@ -0,0 +1,35 @@
+<?php
+
+// Init the hooks of the plugins -Needed
+function plugin_init_autologin() {
+   global $PLUGIN_HOOKS;
+
+   $PLUGIN_HOOKS['csrf_compliant']['autologin'] = true;
+   
+   $PLUGIN_HOOKS['display_login']['autologin'] = 'plugin_autologin_display_login';
+}
+
+// Get the name and the version of the plugin - Needed
+function plugin_version_autologin() {
+   return array(
+       'name'           => 'Auto Login',
+       'version'        => '1.0.0',
+       'author'         => 'Edgard Lorraine Messias',
+       'homepage'       => 'https://github.com/edgardmessias/autologin',
+       'minGlpiVersion' => '0.85'
+   );
+}
+
+// Optional : check prerequisites before install : may print errors or add to message after redirect
+function plugin_autologin_check_prerequisites() {
+   if (version_compare(GLPI_VERSION, '0.85', 'lt')) {
+      echo "This plugin requires GLPI >= 0.85";
+      return false;
+   } else {
+      return true;
+   }
+}
+
+function plugin_autologin_check_config() {
+   return true;
+}
