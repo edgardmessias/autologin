dcea7580c72cb811af27c3255ae41151f6e107bc
Edgard Lorraine Messias <edgardmessias@gmail.com>
2017-01-03 14:50:30 -0200

    Improved for faster redirection;

--- setup.php
+++ setup.php
@@ -2,22 +2,42 @@
 
 // Init the hooks of the plugins -Needed
 function plugin_init_autologin() {
-   global $PLUGIN_HOOKS;
+   global $CFG_GLPI, $PLUGIN_HOOKS;
 
    $PLUGIN_HOOKS['csrf_compliant']['autologin'] = true;
-   
-   $PLUGIN_HOOKS['display_login']['autologin'] = 'plugin_autologin_display_login';
+
+   if (Session::getLoginUserID() && isset($_SERVER['SCRIPT_NAME']) && strpos($_SERVER['SCRIPT_NAME'], 'index.php') !== false) {
+      $REDIRECT = "";
+      if (isset($_POST['redirect']) && (strlen($_POST['redirect']) > 0)) {
+         $REDIRECT = "?redirect=" . rawurlencode($_POST['redirect']);
+      } else if (isset($_GET['redirect']) && strlen($_GET['redirect']) > 0) {
+         $REDIRECT = "?redirect=" . rawurlencode($_GET['redirect']);
+      }
+
+      if ($_SESSION["glpiactiveprofile"]["interface"] == "helpdesk") {
+         if ($_SESSION['glpiactiveprofile']['create_ticket_on_login'] && empty($REDIRECT)) {
+            Html::redirect($CFG_GLPI['root_doc'] . "/front/helpdesk.public.php?create_ticket=1");
+         }
+         Html::redirect($CFG_GLPI['root_doc'] . "/front/helpdesk.public.php$REDIRECT");
+      } else {
+         if ($_SESSION['glpiactiveprofile']['create_ticket_on_login'] && empty($REDIRECT)) {
+            Html::redirect($CFG_GLPI['root_doc'] . "/front/ticket.form.php");
+         }
+         Html::redirect($CFG_GLPI['root_doc'] . "/front/central.php$REDIRECT");
+      }
+      return;
+   }
 }
 
 // Get the name and the version of the plugin - Needed
 function plugin_version_autologin() {
    return array(
-       'name'           => 'Auto Login',
-       'version'        => '1.0.0',
-       'author'         => 'Edgard Lorraine Messias',
-       'license'        => 'GPLv2+',
-       'homepage'       => 'https://github.com/edgardmessias/autologin',
-       'minGlpiVersion' => '0.85'
+      'name'           => 'Auto Login',
+      'version'        => '1.1.0',
+      'author'         => 'Edgard Lorraine Messias',
+      'license'        => 'GPLv2+',
+      'homepage'       => 'https://github.com/edgardmessias/autologin',
+      'minGlpiVersion' => '0.85'
    );
 }
 
