7791af405a82d0d72adf63780a98b48d590c5fec
Edgard Lorraine Messias <edgardmessias@gmail.com>
2017-01-03 23:21:30 -0200

    Added support to "Remember me" in login page;

--- hook.php
+++ hook.php
@@ -7,3 +7,67 @@ function plugin_autologin_install() {
 function plugin_autologin_uninstall() {
    return true;
 }
+
+function plugin_autologin_display_login() {
+   echo "\n<!-- Begin AutoLogin -->\n";
+   echo "<script type=\"text/javascript\">\n";
+   echo "(function() {\n";
+
+   if (version_compare(GLPI_VERSION, '0.90', '>=')) {
+      echo "var passwordLine = document.getElementById('login_password').parentElement;\n";
+      echo "var newLine = document.createElement('p');\n";
+      echo "    newLine.className = 'login_input';\n";
+      echo "    newLine.innerHTML = '<label style=\"color: #FCFCFC;\">"
+      . "<input type=\"checkbox\" name=\"rememberme\" id=\"rememberme\" /> "
+      . __('Remember me', 'autologin') . "</label>';\n";
+      echo "passwordLine.parentElement.insertBefore(newLine, passwordLine.nextSibling);\n";
+   } else {
+      //GLPI <= 0.85
+      echo "var passwordLine = document.getElementById('login_password').parentElement.parentElement;\n";
+
+      echo "var newLine = document.createElement('div');\n";
+      echo "    newLine.className = 'loginrow';\n";
+      echo "    newLine.innerHTML = '<span class=\"loginlabel\"><label>" . __('Remember me', 'autologin') . "</label></span>"
+      . "<span class=\"loginformw\" style=\"margin-right: 140px;\"><input type=\"checkbox\" name=\"rememberme\" id=\"rememberme\"\" /></span>';\n";
+      echo "passwordLine.parentElement.insertBefore(newLine, passwordLine.nextSibling);\n";
+   }
+
+   echo "})();\n";
+   echo "</script>\n";
+   echo "<!-- End AutoLogin -->\n";
+   return true;
+}
+
+function plugin_autologin_init_session() {
+   //If checkbox is not marked, not generate cookie
+   if (!isset($_POST['rememberme'])) {
+      return;
+   }
+
+   //Cookie name (Allow multiple GLPI)
+   $cookie_name = session_name() . '_rememberme';
+   //Remember-me duration after login
+   $cookie_duration = 60 * 60 * 24 * 7; //7 days
+   //Cookie session path
+   $cookie_path = ini_get('session.cookie_path');
+
+   if (Session::getLoginUserID() && !isset($_COOKIE[$cookie_name])) {
+      $uid = Session::getLoginUserID();
+
+      $token = User::getPersonalToken($uid);
+
+      if ($token) {
+         $hash = Auth::getPasswordHash($token);
+
+         $data = json_encode([
+             $uid,
+             $hash,
+             $cookie_duration,
+         ]);
+
+         //Send cookie to browser
+         setcookie($cookie_name, $data, time() + $cookie_duration, $cookie_path);
+         $_COOKIE[$cookie_name] = $data;
+      }
+   }
+}
--- setup.php
+++ setup.php
@@ -6,6 +6,59 @@ function plugin_init_autologin() {
 
    $PLUGIN_HOOKS['csrf_compliant']['autologin'] = true;
 
+   $PLUGIN_HOOKS['display_login']['autologin'] = 'plugin_autologin_display_login';
+   $PLUGIN_HOOKS['init_session']['autologin'] = 'plugin_autologin_init_session';
+
+   //Cookie name (Allow multiple GLPI)
+   $cookie_name = session_name() . '_rememberme';
+
+   //Cookie session path
+   $cookie_path = ini_get('session.cookie_path');
+
+   //For logout, remove COOKIE to prevent automatic login on open index page
+   if (Session::getLoginUserID() && isset($_SERVER['SCRIPT_NAME']) && strpos($_SERVER['SCRIPT_NAME'], 'logout.php') !== false) {
+      setcookie($cookie_name, '', time() - 3600, $cookie_path);
+      return;
+   }
+
+   //If logged, create cookie if not exists
+   if (!Session::getLoginUserID() && isset($_COOKIE[$cookie_name])) {
+
+      $auth_succeded = false;
+
+      $data = json_decode($_COOKIE[$cookie_name], true);
+      if (count($data) === 3) {
+         list ($cookie_id, $cookie_token, $cookie_duration) = $data;
+
+         $token = User::getPersonalToken($cookie_id);
+
+         if ($token !== false && Auth::checkPassword($token, $cookie_token)) {
+            $user = new User();
+            $user->getFromDB($cookie_id); //true if $token is not false
+            //Create fake auth
+            $auth = new Auth();
+            $auth->user = $user;
+            $auth->auth_succeded = true;
+            $auth->extauth = 1;
+            $auth->user_present = $auth->user->getFromDBbyName(addslashes($user->fields['name']));
+            $auth->user->fields['authtype'] = Auth::DB_GLPI;
+
+            Session::init($auth);
+
+            $auth_succeded = $auth->auth_succeded;
+         }
+      }
+
+      //remove COOKIE for invalid cookie
+      //remove COOKIE for invalid token
+      //remove COOKIE for invalid account
+      if (!$auth_succeded) {
+         setcookie($cookie_name, '', time() - 3600, $cookie_path);
+         unset($_COOKIE[$cookie_name]);
+      }
+   }
+
+   //Redirect from login to front page if is authenticated
    if (Session::getLoginUserID() && isset($_SERVER['SCRIPT_NAME']) && strpos($_SERVER['SCRIPT_NAME'], 'index.php') !== false) {
       $REDIRECT = "";
       if (isset($_POST['redirect']) && (strlen($_POST['redirect']) > 0)) {
@@ -25,26 +78,25 @@ function plugin_init_autologin() {
          }
          Html::redirect($CFG_GLPI['root_doc'] . "/front/central.php$REDIRECT");
       }
-      return;
    }
 }
 
 // Get the name and the version of the plugin - Needed
 function plugin_version_autologin() {
    return array(
-      'name'           => 'Auto Login',
-      'version'        => '1.1.0',
-      'author'         => 'Edgard Lorraine Messias',
-      'license'        => 'GPLv2+',
-      'homepage'       => 'https://github.com/edgardmessias/autologin',
-      'minGlpiVersion' => '0.85'
+       'name'           => __('Auto Login', 'autologin'),
+       'version'        => '2.0.0',
+       'author'         => 'Edgard Lorraine Messias',
+       'license'        => 'GPLv2+',
+       'homepage'       => 'https://github.com/edgardmessias/autologin',
+       'minGlpiVersion' => '0.85'
    );
 }
 
 // Optional : check prerequisites before install : may print errors or add to message after redirect
 function plugin_autologin_check_prerequisites() {
    if (version_compare(GLPI_VERSION, '0.85', 'lt')) {
-      echo "This plugin requires GLPI >= 0.85";
+      echo __('This plugin requires GLPI >= 0.85', 'autologin');
       return false;
    } else {
       return true;
